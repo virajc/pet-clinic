7161919b75b2c5b04f1c663d8aaa4b6f7b5daa12
virajc <viraj.chautmal@gmail.com>
2020-10-11 16:53:55 +0530

    Owners controller

---
+++ pet-clinic-web/src/main/java/dev/viraj/petclinic/controller/OwnerController.java
@@ -0,0 +1,13 @@
+package dev.viraj.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+@RequestMapping({"","/","/index","/index.html"})
+	public String listVets() {
+		return "owners/index";
+	}
+}
