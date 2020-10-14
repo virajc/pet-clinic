1d6fd58557f0960163af524a90079b36132b5fbc
virajc <viraj.chautmal@gmail.com>
2020-10-14 10:23:48 +0530

    Diplay List of owners

--- pet-clinic-web/src/main/java/dev/viraj/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/dev/viraj/petclinic/controller/OwnerController.java
@@ -1,13 +1,26 @@
 package dev.viraj.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.viraj.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-@RequestMapping({"","/","/index","/index.html"})
-	public String listVets() {
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		ownerService.findAll().stream().forEach((owner)->{System.out.println(owner.toString());});
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
+
 }
